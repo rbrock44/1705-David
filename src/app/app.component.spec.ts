8e127bf3d2d3739d4de93eff3fa267056acdb5eb
rbrock44 <rbrock444@gmail.com>
2026-07-18 20:53:25 -0500

    upgrade tests and 1 package

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the '1705-David' title`, () => {
+  it(`should have the '1705 David' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('1705-David');
+    expect(app.title).toEqual('1705 David');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, 1705-David');
+    expect(compiled.querySelector('h1')?.textContent).toContain('1705 David Street');
   });
 });
